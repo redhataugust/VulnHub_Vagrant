6591610d6e0090513645fb50c3fc07fac0553354
RedHatAugust <redhataugust@gmail.com>
2021-02-01 08:35:19 -0700

    Changed subdirectory names and modified script

--- VulnHub/2019-12-31_Doomsday-1/setuplab.ps1
+++ VulnHub/2019-12-31_Doomsday-1/setuplab.ps1
@@ -1,4 +1,4 @@
 vagrant box add RedHatAugust/Doomsday-CTF-Control-Server
 vagrant box add RedHatAugust/Doomsday-CTF-Doomsday-Server
-start-process -filepath "vagrant" -workingdirectory ".\ctf" -argumentlist "up"
-start-process -filepath "vagrant" -workingdirectory ".\control" -argumentlist "up"
+start-process -filepath "vagrant" -workingdirectory ".\Control_Server" -argumentlist "up"
+start-process -filepath "vagrant" -workingdirectory ".\Doomserver" -argumentlist "up"
